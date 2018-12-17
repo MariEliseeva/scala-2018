19159948ee0955194122459007983f7daf35da21
Marieliseeva <eliseevamary@mail.ru>
2018-12-17 21:21:10 +0300

    Fix mistakes

--- src/main/scala/ru/hse/spb/Calculator.scala
+++ src/main/scala/ru/hse/spb/Calculator.scala
@@ -2,50 +2,73 @@ package ru.hse.spb
 
 class Calculator() extends ExpBaseVisitor[Int] {
   override def visitExpression(ctx: ExpParser.ExpressionContext): Int = {
-    if (ctx.Literal() != null) {
-      ctx.Literal().getText match {
-        case "true" => return 1
-        case "false" => return 0
-        case integer => return integer.toInt
-      }
+    ctx.Literal() match {
+      case null => visitNonLiteralExpression(ctx)
+      case literal => Calculator.getLiteral(literal.getText)
     }
-
-    if (ctx.expression().size == 1) {
-      if (ctx.operation == null) {
-        return visitExpression(ctx.expression(0))
-      }
-      ctx.operation.getText match {
-        case "-" => return - visit(ctx.expression(0))
-        case "!" => return toNumber(visit(ctx.expression(0)) == 0)
-      }
-      throw new UnsupportedOperationException()
-    }
-    if (ctx.expression().size == 2) {
-      val left = visit(ctx.expression(0))
-      val right = visit(ctx.expression(1))
-       ctx.operation.getText match {
-        case "+" => return left + right
-        case "-" => return left - right
-        case "*" => return left * right
-        case "/" => return left / right
-        case "%" => return left % right
-        case ">" => return toNumber(left > right)
-        case  "<" => return toNumber(left < right)
-        case ">=" => return toNumber(left >= right)
-        case  "<=" => return toNumber(left <= right)
-        case  "==" => return toNumber(left == right)
-        case  "!=" => return toNumber(left != right)
-        case "&&" => return toNumber(left != 0 && right != 0)
-        case "||" => return toNumber(left != 0 || right != 0)
-      }
-    }
-    throw new UnsupportedOperationException()
   }
 
-  private def toNumber(value: Boolean): Int = {
-    if (value) {
-      return 1
+  private def visitNonLiteralExpression(ctx: ExpParser.ExpressionContext): Int = {
+    ctx.expression().size match {
+      case 1 =>
+        ctx.operation match {
+          case null => visitExpression(ctx.expression(0))
+          case operation => Calculator.unaryOperation(operation.getText, visit(ctx.expression(0)))
+        }
+      case 2 => binaryOperation(ctx)
     }
-    0
   }
+
+  private def binaryOperation(ctx: ExpParser.ExpressionContext): Int = {
+    val left = visit(ctx.expression(0))
+    val right = visit(ctx.expression(1))
+    ctx.operation.getText match {
+      case "+" => left + right
+      case "-" => left - right
+      case "*" => left * right
+      case "/" => left / right
+      case "%" => left % right
+      case otherOperation => Calculator.logicalOperation(left, right, otherOperation)
+    }
+  }
+}
+object Calculator {
+  private final val TRUE: Int = 1
+  private final val FALSE: Int = 0
+
+  private def unaryOperation(operation: String, operand: Int): Int = {
+    operation match {
+      case "-" => -operand
+      case "!" => Calculator.toNumber(operand == Calculator.FALSE)
+      case _ => throw new UnsupportedOperationException()
+    }
+  }
+
+  private def logicalOperation(left: Int, right: Int, operation: String): Int = {
+    Calculator.toNumber(
+      operation match {
+        case ">" => left > right
+        case "<" => left < right
+        case ">=" => left >= right
+        case "<=" => left <= right
+        case "==" => left == right
+        case "!=" => left != right
+        case "&&" => isTrue(left) && isTrue(right)
+        case "||" => isTrue(left) || isTrue(right)
+        case _ => throw new UnsupportedOperationException()
+      }
+    )
+  }
+
+  private def getLiteral(literal: String): Int = {
+    literal match {
+      case "true" => Calculator.TRUE
+      case "false" => Calculator.FALSE
+      case integer => integer.toInt
+    }
+  }
+
+  private def toNumber(value: Boolean): Int = if (value) TRUE else FALSE
+
+  private def isTrue(value: Int): Boolean = if (value != FALSE) true else false
 }
